d24974b41ce9aefab8e62bc2f0e24e68689805e9
Yogesh Gangwar <yogesh.gangwar@ad.infosys.com>
2022-11-11 14:51:47 +0530

    added facebook login

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,14 @@
 Rails.application.routes.draw do
-  
-  
   root "user#signin"
   get "/signin", to: "user#signin", as: "signin"
   post "/login", to: "user#login", as: "login"
-   get "/register", to: "user#register", as: "register"
+  post "/fbuserlogin", to: "user#fbuserlogin", as: "fbuserlogin"
+  get "/register", to: "user#register", as: "register"
   post "/registeruser", to: "user#registeruser", as: "registeruser"
-  get "/logout", to:"user#logout",as:"userlogout"
+  get "/logout", to: "user#logout", as: "userlogout"
 
   get "/userprofile", to: "index#userprofile", as: "userprofile"
-  delete '/deletebooks/:id' , to:"index#deletebooks" ,as: "deletex"
-  get '/newbook(/:id)',to:"index#newbook", as:"newbook"
-  post "/addbook",to:"index#addbook", as:"addbook"
+  delete "/deletebooks/:id", to: "index#deletebooks", as: "deletex"
+  get "/newbook(/:id)", to: "index#newbook", as: "newbook"
+  post "/addbook", to: "index#addbook", as: "addbook"
 end
